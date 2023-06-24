04c28a05ffbe90c34cb6c2156cfca5c92ebc70e3
ecemusaa <ecemusaa@gmail.com>
2023-06-24 16:13:53 +0800

    单测添加变量类型

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -2,49 +2,49 @@ import { effect } from "../effect"
 import { reactive } from "../reactive"
 
 describe('effect', () => {
-    // it('core', () => {
-    //     const obj = {
-    //         num: 1,
-    //         name: 'abc'
-    //     }
-    //     const rxObj = reactive(obj)
+    it('core', () => {
+        const obj = {
+            num: 1,
+            name: 'abc'
+        }
+        const rxObj = reactive(obj)
 
-    //     let num
-    //     effect(() => {
-    //         num = rxObj.num + 1
-    //     })
+        let num
+        effect(() => {
+            num = rxObj.num + 1
+        })
 
-    //     expect(num).toBe(2)
+        expect(num).toBe(2)
 
-    //     let num1
-    //     effect(() => {
-    //         num1 = rxObj.num + 1
-    //     })
-    //     rxObj.num++
-    //     expect(num).toBe(3)
-    //     expect(num1).toBe(3)
-    //     rxObj.name = 'def'
-    // })
+        let num1
+        effect(() => {
+            num1 = rxObj.num + 1
+        })
+        rxObj.num++
+        expect(num).toBe(3)
+        expect(num1).toBe(3)
+        rxObj.name = 'def'
+    })
 
-    // it('runner', () => {
-    //     let num = 1
-    //     const runner = effect(() => {
-    //         num++
-    //         return 'effect'
-    //     })
+    it('runner', () => {
+        let num = 1
+        const runner = effect(() => {
+            num++
+            return 'effect'
+        })
 
-    //     expect(num).toBe(2)
+        expect(num).toBe(2)
 
-    //     const res = runner()
-    //     expect(num).toBe(3)
-    //     expect(res).toBe('effect')
-    // })
+        const res = runner()
+        expect(num).toBe(3)
+        expect(res).toBe('effect')
+    })
 
     it('scheduler', () => {
         const obj = {num: 1}
         const rxObj = reactive(obj)
         let num
-        let run
+        let run: Function | undefined
         const scheduler = jest.fn(() => {
             run = runner
         })
@@ -59,8 +59,9 @@ describe('effect', () => {
         rxObj.num++
         expect(num).toBe(1)
         expect(scheduler).toHaveBeenCalledTimes(1)
-
-        run()
+        if (run) {
+            run()
+        }
         expect(num).toBe(2)
     })
 })
